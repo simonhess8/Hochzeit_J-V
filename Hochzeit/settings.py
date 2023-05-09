78ff9744c3c3b385deaa485625aeae3e0570a576
simonhess8 <hess.simon.99@gmx.de>
2023-05-09 18:05:43 +0200

    new local_settings

--- Hochzeit/settings.py
+++ Hochzeit/settings.py
@@ -126,3 +126,9 @@ MEDIA_URL = '/media/'
 # https://docs.djangoproject.com/en/4.1/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
+
+
+try:
+    from .local_settings import *
+except ImportError:
+    print("Looks like no local file, you must be in production")
